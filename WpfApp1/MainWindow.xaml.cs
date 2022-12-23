bf41b09eac706ced74eef48f6e14357591ed43cb
RaulH <rulisespinosahot@gmail.com>
2022-12-22 18:17:34 -0600

    v2

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+    
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                ;
+                var http_c = new HttpClient();
+                var json = await http_c.GetStringAsync("https://api.github.com/users");
+                var x = JsonConvert.DeserializeObject<List<model>>(json);
+                foreach (model m in x)
+                {
+                    btn1.IsEnabled = false;
+                    txt1.Text = m.login + "‘|’" + m.id + "‘|’" + m.node_id;
+                }
+            }
+            catch (Exception )
+            {
+
+            }
+            //using (HttpClient clien = new HttpClient())
+            //{
+            //    var res = await clien.GetAsync("https://api.github.com/users");
+
+            //    res.EnsureSuccessStatusCode();
+            //    if (res.IsSuccessStatusCode)
+            //    {
+            //      btn1.IsEnabled= false;
+            //      txt1.Text = await res.Content.ReadAsStringAsync();
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/model.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public  class model
+    {
+        public string login { get; set; } = string.Empty;
+
+        public string id { get; set; } = string.Empty;
+
+        public string node_id { get; set; } = string.Empty;
+
+        public string avatar_url { get; set; } = string.Empty;
+
+
+        public string gravatar_id { get; set; } = string.Empty;
+
+
+        public string url { get; set; } = string.Empty;
+
+        public string html_url { get; set; } = string.Empty;
+
+
+        public string followers_url { get; set; } = string.Empty;
+
+
+        public string following_url { get; set; } = string.Empty;
+
+
+        public string gists_url { get; set; } = string.Empty;
+
+
+        public string starred_url { get; set; } = string.Empty;
+
+
+        public string subscriptions_url { get; set; } = string.Empty;
+
+
+        public string organizations_url { get; set; } = string.Empty;
+
+
+        public string repos_url { get; set; } = string.Empty;
+
+
+        public string events_url { get; set; } = string.Empty;
+
+
+        public string received_events_url { get; set; } = string.Empty;
+
+
+        public string type { get; set; } = string.Empty;
+
+
+        public string site_admin { get; set; } = string.Empty;
+    }
+}
